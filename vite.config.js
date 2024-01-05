153cda9dd686a0851a145efae4dc17b63f28a0b1
elisaul batista <elisaulbatista@pursuit.org>
2024-01-04 21:25:55 -0500

    added

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build', 
+    assetsDir: 'assets', 
+    sourcemap: true, 
+    minify: 'terser', 
+  },
+
+  server: {
+    port: 3000, 
+    open: true, 
+  },
 })
